77e9d1a2198d5aee9b40c2db22266d6419b7bbca
xval <xavier.valcanto@telefonica.com>
2015-09-01 11:05:52 +0200

    Update orchestrator_utils.py

--- orchestrator_utils.py
+++ orchestrator_utils.py
@@ -28,7 +28,7 @@ __author__ = 'avega'
 import json
 import requests
 from requests.exceptions import RequestException
-from common_utils.iot_logger import get_logger
+from iotqatools.common_utils.iot_logger import get_logger
 
 
 class Orchestrator(object):
